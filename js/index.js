1b0d5576db57ba36e0af87e38a30a02977c72659
Matthewmob <game1000@hotmail.co.uk>
2016-08-25 21:51:12 +1000

    Added generate strat feature

--- js/index.js
+++ js/index.js
@@ -1,3 +1,47 @@
 if (document.location.hash == "" || document.location.has == "#") {
 	document.location.hash = "#menu";
 }
+
+function copyStrat() {
+	document.getElementById("stratCopy").select();
+	try {
+		document.execCommand("copy");
+		document.querySelector("#gen .button.copy a").innerHTML = "Copied!";
+	} catch (err) {
+		console.log("Failed to copy");
+	}
+}
+
+var last = -1;
+function genStrat() {
+	var getRand = Math.floor(Math.random() * strats.length) + 0;
+	var strat = strats[getRand];
+	if (getRand != last) {
+		document.getElementById("stratName").innerHTML = strat.header;
+		document.getElementById("stratText").innerHTML = strat.text;
+		document.getElementById("stratAuthor").innerHTML = strat.author;
+		document.getElementById("stratCopy").value = strat.text;
+	} else {
+		genStrat();
+	}
+
+	document.querySelector("#gen .button.copy a").innerHTML = "Copy to Clipboard";
+	last = getRand;
+}
+
+document.querySelector("#gen .button.copy a").addEventListener("click", copyStrat);
+document.querySelector("#gen .button.gen a").addEventListener("click", genStrat);
+
+genStrat();
+
+document.addEventListener("keypress", function(event) {
+	var e = event.keyCode;
+	if (e == 32) {
+		genStrat();
+		document.querySelector("#genContent").classList.remove("fadeIn");
+		setTimeout(function() {
+			document.querySelector("#genContent").classList.add("fadeIn");
+		}, 1);
+		console.log("Pressed");
+	}
+});
---
+++ js/strats.js
@@ -0,0 +1,97 @@
+var strats = new Array();
+
+/*
+
+strats.push({
+	header: "",
+	text: "",
+	author: ""
+});
+
+*/
+
+strats.push({
+	header: "Gotta Go Fast",
+	text: "Play with 6 Lucios and do a full marathon to the objective, non-stop speed bosts.",
+	author: "MatthewMob"
+});
+strats.push({
+	header: "Rise of the Planet of the Apes",
+	text: "Play with 6 Winstons and use only your melee.",
+	author: "MEMELORD"
+});
+strats.push({
+	header: "Blind Run",
+	text: "Play with 3 Hanzos and 3 Junkrats. No one looks directly at the enemy. Hanzos use their sonic arrow for vision, and Junkrats fire around corners to kill the enemies.",
+	author: "DuckLeg"
+});
+strats.push({
+	header: "I'll Cover You",
+	text: "Pharahs my only attack targets on the ground and soldiers may only attack targets in the air or on high ground.",
+	author: "DudeMIC"
+});
+strats.push({
+	header: "NOSCOPE",
+	text: "Play as Widowmaker and only use SMG.",
+	author: "Volcan1c"
+});
+strats.push({
+	header: "It's a Trap!",
+	text: "Play with four Symmetras, one Roadhog and one Junkrat. Symmetras will set up a kill room of turrets, the Junkrat will place his trap in it, and the Roadhog will hook people in the room.",
+	author: "KillKAPPS"
+});
+strats.push({
+	header: "Play of the Game!",
+	text: "Every member of your team must try to get play of the game no matter what. Winning does not matter.",
+	author: "Volcan1c"
+});
+strats.push({
+	header: "Snow White",
+	text: "Play with one Mercy and 5 Torbjorns. Mercy is snow white and must protect her dwarves and vice versa.",
+	author: "Volcan1c"
+});
+strats.push({
+	header: "Single Life",
+	text: "Once you or a member of your team dies they must switch heroes and cannot pick the same hero twice.",
+	author: "TheDarkWolfe"
+});
+strats.push({
+	header: "Hook, Line, and Sinker",
+	text: "Your whole team will hide around a corner. One member of your team will bait people around the corner, only for that enemy to be slaughtered by your team.",
+	author: "Nuuka"
+});
+strats.push({
+    header: "Yin Yang",
+    text: "Playing as Zenyatta the whole team can use their weapon, but half of the team must only use Orb of Discord and half must only use Orb of Harmony.",
+    author: "Alex"
+});
+strats.push({
+    header: "Lane Control",
+    text: "Use Mei's wall to force your enemies into the unstoppable barrage being laid down by each of your Bastions.",
+    author: "Alex"
+});
+strats.push({
+    header: "Reverse Gun Game",
+    text: "Each pick your category (Tank/Support/Offense/Defense). Each time you die you must move to the next character in the category.",
+    author: "Alex"
+});
+strats.push({
+    header: "One In The Chamber",
+    text: "All players are Mcree and all have to reload after each bullet, no fan the hammer.",
+    author: "BT160613"
+});
+strats.push({
+    header: "Rat Trap",
+    text: "Playing as Junkrat, move around as a team. You can only fire at an enemy once they have been trapped by at least 1 player.",
+    author: "Alex"
+});
+strats.push({
+	header: "Pinball",
+	text: "Everyone must play Lucio and only use their right-click.",
+	author: "MatthewMob"
+});
+strats.push({
+    header: "When In Rome",
+    text: "Play with four Reinhardts and two Hanzos. Shield your archers as their arrows block out the sun. Have three Reinhardts each covering a direction and the fourth cover the top. Have your archers shoot at the enemy as you slowly move forward.",
+    author: "Toyos"
+});
